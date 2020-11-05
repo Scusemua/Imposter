e86afca3ee38147eb9e33997aacd355eabfae379
Scusemua <benrcarver@gmail.com>
2020-11-05 12:48:18 -0500

    More updates to Timer script and VotingUI.

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,17 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
-{
-    private enum Phase
+{    public enum Phase
     {
         DISCUSSION,
         TRANSITION, // Brief pause before beginning the voting phase. 
         VOTING 
     }
+
+    public float TimeRemaining;
+
+    public GameOptions GameOptions;
+
+    public TextMeshProUGUI TimerText;
+    public TextMeshProUGUI PhaseLabel;
 
     /// <summary>
     /// Length of transition phase, which occurs briefly between DISCUSSION and VOTING.
@@ -20,17 +29,11 @@ public class Timer : MonoBehaviour
 
     private Phase currentPhase;
 
-    public float timeRemaining;
-
-    public GameOptions gameOptions;
-
-    public TextMeshProUGUI timerText;
-
     // Since apparently Color has no static orange property. 
     private Color orange = new Color(255, 140, 0);
 
-    private float orangeThresholdPercent = 0.5f;
-    private float redThresholdPercent = 0.25f;
+    private float orangeThresholdPercent = 0.525f; // A little above 50% to ensure color change is visible by 50%.
+    private float redThresholdPercent = 0.275f;      // A little above 25% to ensure color change is visible by 25%.
 
     private float orangeThresholdVoting;
     private float redThresholdVoting;
@@ -41,16 +44,18 @@ public class Timer : MonoBehaviour
     void Start()
     {
         currentPhase = Phase.DISCUSSION;
-        gameOptions = GameOptions.singleton;
+        GameOptions = GameOptions.singleton;
 
-        timeRemaining = gameOptions.discussionPeriod;
+        TimeRemaining = GameOptions.discussionPeriod;
 
         // Calculate these once at the beginning so we don't have to recalculate them everytime.
-        orangeThresholdVoting = gameOptions.votingPeriod * orangeThresholdPercent;
-        redThresholdVoting = gameOptions.votingPeriod * redThresholdPercent;
+        orangeThresholdVoting = GameOptions.votingPeriod * orangeThresholdPercent;
+        redThresholdVoting = GameOptions.votingPeriod * redThresholdPercent;
 
-        orangeThresholdDiscussion = gameOptions.discussionPeriod * orangeThresholdPercent;
-        redThresholdDiscussion = gameOptions.discussionPeriod * redThresholdPercent;
+        orangeThresholdDiscussion = GameOptions.discussionPeriod * orangeThresholdPercent;
+        redThresholdDiscussion = GameOptions.discussionPeriod * redThresholdPercent;
+
+        PhaseLabel.text = "Discuss";
     }
 
     private Color getColorFromTimeRemaining(float timeRemaining)
@@ -80,34 +85,40 @@ public class Timer : MonoBehaviour
     void Update()
     {
         // While the timer is still active, decrement by the amount of time that has passed.
-        if (timeRemaining > 0)
+        if (TimeRemaining > 0)
         {
-            timeRemaining -= Time.deltaTime;
-            timerText.text = ((int)timeRemaining).ToString();
+            TimeRemaining -= Time.deltaTime;
 
-            timerText.color = getColorFromTimeRemaining(timeRemaining);
+            // Timer text is blank during transition phase.
+            if (currentPhase != Phase.TRANSITION)
+                TimerText.text = ((int)TimeRemaining).ToString();
+
+            TimerText.color = getColorFromTimeRemaining(TimeRemaining);
         }
 
         // Timer has expired. Transition to next phase. 
-        else if (timeRemaining <= 0)
+        else if (TimeRemaining <= 0)
         {
-            timeRemaining = 0;
-            timerText.text = "0";
-            timerText.color = Color.white;
+            TimeRemaining = 0;
+            TimerText.text = "0";
+            TimerText.color = Color.white;
             
             // Current phase is DISCUSSION. Need to transition to TRANSITION.
             if (currentPhase == Phase.DISCUSSION)
             {
                 Debug.Log("Timer has finished period " + currentPhase + ". Transitioning to phase " + Phase.TRANSITION);
                 currentPhase = Phase.TRANSITION;
-                timeRemaining = 1.0f;
+                PhaseLabel.text = "Vote";
+                TimeRemaining = 1.0f;
+                TimerText.text = "";
             }
             // Current phase is TRANSITION. Need to transition to VOTING.
             else if (currentPhase == Phase.TRANSITION)
             {
                 Debug.Log("Timer has finished period " + currentPhase + ". Transitioning to phase " + Phase.VOTING);
                 currentPhase = Phase.VOTING;
-                timeRemaining = gameOptions.votingPeriod;
+                PhaseLabel.text = "Vote";
+                TimeRemaining = GameOptions.votingPeriod;
             }
             // Current phase is VOTING. We're done.
             else
