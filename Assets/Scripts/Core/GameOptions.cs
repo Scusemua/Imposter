d442ce3708bc3d5612169407bae384c30e0d2ea7
Scusemua <benrcarver@gmail.com>
2020-11-19 22:54:20 -0500

    Working on GameOptions UI

--- Assets/Scripts/Core/GameOptions.cs
+++ Assets/Scripts/Core/GameOptions.cs
@@ -7,6 +7,7 @@ public class GameOptions : MonoBehaviour
 {
     public int NumPlayers;
 
+    [Header("Movement")]
     /* Movement related. */
     public float PlayerSpeed = 5;
     public bool SprintEnabled = true;               // Is the sprint mechanic enabled?
@@ -14,6 +15,7 @@ public class GameOptions : MonoBehaviour
     public float CrewmateSprintDuration = 10;       // How long can crewmates sprint?
     public float ImposterSprintDuration = 15;       // How long can imposters sprint?
 
+    [Header("Footprints")]
     /* Footprint related. */
     public bool BloodyFootprintsEnabled;    // Are there bloody footprints after killing?
     public bool BloodyFootprintsOnKillOnly; // If False, any player will have bloody footprints after walking over a dead body.
@@ -22,31 +24,37 @@ public class GameOptions : MonoBehaviour
     public bool FootprintsDisappear;        // Do the footprints eventually disappear?       
     public float FootprintDuration;         // How long do footprints stick around before disappearing?
 
+    [Header("Killing")]
     /* Killing related. */
     public float KillIntervalStandard;              // How long inbetween kills by standard imposter?
     public float KillIntervalAssassin;              // How long do assassins wait inbetween kills?
     public float KillDistanceStandard = 0.5f;       // Minimum distance required between crewmate and standard imposter for kill to be available.
     public float KillDistanceAssassin;              // Minimum distance required between crewmate and assassin imposter for kill to be available.
 
+    [Header("Tasks")]
     /* Task-related settings. */
     public int NumCommonTasks;              // How many tasks do ALL players have?
     public int NumShortTasks;               // How many short-length tasks does each player get?
     public int NumLongTasks;                // How many long-length tasks does each player get?
 
+    [Header("Voting & Discussion")]
     /* Voting and discussion related. */
     public int NumEmergencyMeetings;        // How many emergency players may each player call?
     public float MeetingCooldown;           // How long must players wait inbetween emergency meetings?
     public float DiscussionPeriodLength;    // How long do players have to discuss before voting begins?
     public float VotingPeriodLength;        // How long do players have to cast their votes once voting begins?
-    
+
+    [Header("Emergencies")]
     /* Emergency related. */
     public float EmergencyCooldownStandard; // How long must regular imposters wait inbetween causing emergencies?
     public float EmergencyCooldownSaboteur; // How long must Saboteurs wait inbetween causing emergencies?
     public float EmergencyMeetingCooldown;  // How much time must pass before the next emergency meeting can be called?
 
+    [Header("Win Conditions")]
     /* Win Conditions */
     public bool ImpostersMustKillAllCrewmates;
 
+    [Header("Roles")]
     /* Role related */
     public bool SaboteurEnabled;                // Is the "Saboteur" imposter role enabled?
     public bool AssassinEnabled;                // Is the "Assassin" imposter role enabled?
@@ -57,6 +65,7 @@ public class GameOptions : MonoBehaviour
     public int NumberOfImposters;               // How many imposters are there?
     public float SheriffScannerCooldown;        // How long must the sheriff wait inbetween uses of his/her scanner?
 
+    [Header("Vision")]
     /* Vision related. */
     public int CrewmateVision;              // How far can crewmates see?
     public int ImposterVision;              // How far can imposters see?
@@ -64,6 +73,7 @@ public class GameOptions : MonoBehaviour
     /* Dark-mode Related */
     public bool DarkModeEnabled;
 
+    [Header("Weapons")]
     /* Gun Related */
     public bool GunsEnabled;
     public bool CrewmatesHaveGuns;
@@ -87,6 +97,7 @@ public class GameOptions : MonoBehaviour
     public float Range;
     public float ProjectileDamage;  // How much damage each individual projectile does.
 
+    [Header("Debug")]
     /* Debug */
     public bool disableWinChecking;
 
