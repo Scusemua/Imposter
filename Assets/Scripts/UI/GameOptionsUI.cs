c8f0e787f6a7e870fbb17ac6d1ea225d1bc38968
Scusemua <benrcarver@gmail.com>
2020-11-19 23:13:30 -0500

    Working more on GameOptions UI

---
+++ Assets/Scripts/UI/GameOptionsUI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOptionsUI : MonoBehaviour
+{
+    [Header("UI Elements")]
+    public GameObject WeaponScrollviewContent;
+
+    [Header("UI Prefabs")]
+    public GameObject WeaponGroupHeaderPrefab;
+    public GameObject WeaponEntryPrefab;
+
+    [Header("Data")]
+    public Gun[] WeaponPrefabs;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
